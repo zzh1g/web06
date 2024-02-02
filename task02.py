bf86c9230e1a4f3bcf5d35f92f4c081620d9df8f
Ivan_n+2k <ryzptdbdfy@mail.ru>
2024-02-02 20:14:09 +0300

    task_2

--- task02.py
+++ task02.py
@@ -1,10 +1,8 @@
-import os
 import sys
 
-import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget
 from api_yandex_maps import getImage
 from task_01_ui import Ui_MainWindow
 
@@ -14,27 +12,36 @@ class Example(QWidget, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.initUI()
-        self.show_image.clicked.connect(self.show_image_)
+        self.show_image.clicked.connect(self.push)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageDown:
-            self.spn_ = str(float(self.spn_) / 2)
+            self.spn = str(float(self.spn) / 2)
             self.show_image_()
         elif event.key() == Qt.Key_PageUp:
-            self.spn_ = str(float(self.spn_) * 2)
+            self.spn = str(float(self.spn) * 2)
             self.show_image_()
 
     def show_image_(self):
-        longitude = self.longitude.text()
-        lattitude = self.lattitude.text()
-        self.spn_ = self.spn.text()
-        type_maps = self.type_maps.currentText()
-        picture = getImage(','.join((longitude, lattitude)), ','.join((self.spn_, self.spn_)), type_maps)
+        picture = getImage(','.join((self.longitude, self.lattitude)), ','.join((self.spn, self.spn)),
+                           self.type_maps)
         self.pixmap = QPixmap.fromImage(QImage.fromData(picture))
         self.image.setPixmap(self.pixmap)
+        self.setFocus()
+
+    def push(self):
+        self.longitude = self.longitude.text()
+        self.lattitude = self.lattitude.text()
+        self.spn = self.spn.text()
+        self.type_maps = self.type_maps.currentText()
+        self.show_image_()
 
     def initUI(self):
         self.setWindowTitle('Отображение карты')
+        self.longitude = self.longitude.text()
+        self.lattitude = self.lattitude.text()
+        self.spn = self.spn.text()
+        self.type_maps = self.type_maps.currentText()
         self.show_image_()
 
 
